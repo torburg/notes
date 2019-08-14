97edeeabb19ded199e1f61e6f81f0bad224e2af1
Maksim Torburg <torburg@yandex.ru>
2019-08-15 02:13:27 +0300

    NoteView created

---
+++ Notes/NoteViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NoteViewController.swift
+//  Notes
+//
+//  Created by Maksim Torburg on 15/08/2019.
+//  Copyright © 2019 Maksim Torburg. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+//    @IBOutlet weak var title: UITextField!
+    @IBOutlet weak var noteText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
